46e835c502c80932cd078818415a5dcac210c57c
Ken Lee Shu Ming <ken.lee@shopee.com>
2018-11-02 19:09:45 +0800

    askjdh

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  rules: {
+    "no-bitwise": 0,
+    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+    "arrow-parens": ["error", "as-needed"],
+    "no-await-in-loop": 0,
+    "no-param-reassign": 0,
+    "object-curly-newline": 0,
+    "implicit-arrow-linebreak": 0,
+  },
+};
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const {
   State,
   u16,
   signed8,
+  u16Tou8,
 } = require('./state');
 
 const { err, log } = require('./logger');
@@ -17,6 +18,16 @@ const keyPressed = [null, true];
 KeyUtils.handleKeyPress(keyPressed);
 
 const memory = RomUtils.loadRom(ROM_FILE);
+
+const BCDEHLA_TABLE = {
+  0x04: 'B',
+  0x0C: 'C',
+  0x14: 'D',
+  0x1C: 'E',
+  0x24: 'H',
+  0x2C: 'L',
+  0x3C: 'A',
+};
 
 const RST_ADDRESS = {
   0xC7: 0x00,
@@ -43,16 +54,79 @@ const gameloop = state => {
   const opCode = memory[PC];
   log(`PC: 0x${PC.toString(16).padStart(4, 0)} OPCODE: ${opCode.toString(16)}`);
   switch (opCode) {
-    case 0x05: // DEC B
-      state.B = (state.B - 1) && 0xFF;
-      addPC(1);
+    case 0x08: // LD (nn), SP
+      {
+        const [H, L] = u16Tou8(state.SP);
+        memory[state.PC + 2] = H;
+        memory[state.PC + 1] = L;
+        addPC(3);
+      }
       break;
-    case 0x08: // L D(nn), SP
-      memory[state.PC + 2] = state.SP & 0xFF00;
-      memory[state.PC + 1] = state.SP & 0x00FF;
-      addPC(3);
+
+      // INC
+    case 0x04: // B
+    case 0x0C: // C
+    case 0x14: // D
+    case 0x1C: // E
+    case 0x24: // H
+    case 0x2C: // L
+    case 0x3C: // A
+      {
+        const register = BCDEHLA_TABLE[opCode];
+        // TODO: might have overflow/flags
+        const v = state.getRegister(register) + 1;
+        state.setRegister(register, v);
+        addPC(1);
+      }
+      break;
+      // DEC
+    case 0x05: // B
+    case 0x0D: // C
+    case 0x15: // D
+    case 0x1D: // E
+    case 0x25: // H
+    case 0x2D: // L
+    case 0x3D: // A
+      {
+        const register = BCDEHLA_TABLE[opCode - 1];
+        // TODO: might have overflow/flags
+        const v = state.getRegister(register) - 1;
+        state.setRegister(register, v);
+        addPC(1);
+      }
+      break;
+
+      // LD R, n
+    case 0x06: // LD B, n
+    case 0x0e: // LD C, n
+    case 0x16: // LD D, n
+    case 0x1e: // LD E, n
+    case 0x26: // LD H, n
+    case 0x2e: // LD L, n
+    case 0x3e: // LD A, n
+      {
+        const n = memory[PC + 1];
+        const register = BCDEHLA_TABLE[opCode - 2];
+        state.setRegister(register, n);
+        addPC(2);
+      }
+      break;
+    case 0x36: // LD (HL), n
+      {
+        const n = memory[PC + 1];
+        const HL = state.getRegister('HL');
+        memory[HL] = n;
+        addPC(2);
+      }
       break;
     case 0x20: // JR NZ, r
+    case 0xC2:
+    case 0xCA:
+    case 0x28:
+    case 0x30:
+    case 0xD2:
+    case 0x38:
+    case 0xDA:
       {
         const r = signed8(memory[PC + 1]);
         log('JR NZ, r');
@@ -75,7 +149,7 @@ const gameloop = state => {
     case 0x22: // LD [HL+], A
     case 0x32: // LD [HL-], A
       {
-        let HL = u16(state.H, state.L);
+        let HL = state.getRegister('HL');
         memory[HL] = state.A;
         HL += opCode === 0x22 ? 1 : -1;
         state.setRegister('HL', HL);
@@ -90,7 +164,7 @@ const gameloop = state => {
         addPC(1);
       }
       break;
-    case 0x31:
+    case 0x31: // LD SP, nn
       setSP(u16(memory[PC + 2], memory[PC + 1]));
       addPC(3);
       break;
@@ -108,16 +182,25 @@ const gameloop = state => {
     case 0xDF:
     case 0xEF:
     case 0xFF:
-      memory[state.SP - 1] = PC & 0xFF00;
-      memory[state.SP - 2] = PC & 0x00FF;
-      addSP(-2);
-      setPC(RST_ADDRESS[opCode]);
+      {
+        const [H, L] = u16Tou8(PC);
+        memory[state.SP - 1] = H;
+        memory[state.SP - 2] = L;
+        addSP(-2);
+        setPC(RST_ADDRESS[opCode]);
+      }
+      break;
+
+    case 0xE2: // LD (0xFF00 + C), A
+      memory[0xFF00 + state.C] = state.A;
+      addPC(1);
       break;
     case 0xCB: // Bit Operations
       CB(memory[PC + 1], state);
       log('CB', memory[PC + 1].toString(16));
       addPC(2);
       break;
+
     default:
       err('\n\nUnhandled OpCode:', opCode.toString(16));
       throw new Error();

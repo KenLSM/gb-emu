8c5245c832e1d0d057adbc8d07411bbca601a915
Ken Lee Shu Ming <ken.lee@shopee.com>
2018-11-07 19:32:28 +0800

    ! add demo

--- src/b.js
+++ src/b.js
@@ -23,14 +23,23 @@ program.enableMouse();
 program.hideCursor();
 program.clear();
 
-program.move(1, 1);
-program.bg('black');
-program.write('Hello world', 'blue fg');
-program.setx((program.cols / 2 | 0) - 4);
-program.write(`col ${program.cols}`);
-program.down(1);
-program.write(`rows ${program.rows}`);
-program.down(5);
-program.write('Hi again!');
-program.bg('!black');
-program.feed();
+
+const main = async () => {
+  for (let i = 0; i < 160; i++) {
+    program.clear();
+    program.move(i, i);
+    program.bg('black');
+    program.write('Hello world', 'blue fg');
+    program.setx((program.cols / 2 | 0) - 4);
+    program.write(`col ${program.cols}`);
+    program.down(1);
+    program.write(`rows ${program.rows}`);
+    program.down(5);
+    program.write('Hi again!');
+    program.bg('!black');
+    program.feed();
+    await new Promise(p => setTimeout(p, 10));
+  }
+};
+
+main();
--- src/d.js
+++ src/d.js
@@ -2,15 +2,19 @@ const Canvas = require('drawille');
 
 const c = new Canvas(160, 160);
 
-for (let i = 0; i < 160; i++) {
-  c.set(i, i);
-  c.set(i, 0);
-  c.set(i, 1);
+const main = async () => {
+  for (let i = 0; i < 160; i++) {
+    c.set(i, i);
+    c.set(i, 0);
+    c.set(i, 1);
 
-  c.set(0, i);
-  c.set(1, i);
-  c.set(i, 160 - i);
-  c.set(160 - i, i);
+    c.set(0, i);
+    c.set(1, i);
+    c.set(i, 160 - i);
+    c.set(160 - i, i);
+    // process.stdout.write('\033c');
+    process.stdout.write(c.frame());
+    await new Promise(p => setTimeout(p, 10));
+  }
 }
-
-process.stdout.write(c.frame());
+main();

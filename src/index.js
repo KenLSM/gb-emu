b73aa7f82d1b29cfc2a68481d4a8b99285d3a7ae
Ken Lee Shu Ming <ken.lee@shopee.com>
2018-11-09 18:53:59 +0800

    ! add LD r8, (BCDEHLA)

--- src/index.js
+++ src/index.js
@@ -21,6 +21,12 @@ KeyUtils.handleKeyPress(keyPressed);
 
 const memory = RomUtils.loadRom(ROM_FILE);
 
+const BDH_TABLE = {
+  4: 'B',
+  5: 'D',
+  6: 'H',
+};
+
 const CELA_TABLE = {
   4: 'C',
   5: 'E',
@@ -69,7 +75,11 @@ const gameloop = state => {
   } = state;
   const opCode = memory[PC];
   log(`PC: 0x${PC.toString(16).padStart(4, 0)} OPCODE: ${opCode.toString(16)}`);
-
+  if (PC === 0x68) {
+    log(memory[0xFF44]);
+    memory[0xFF44] = 0x90; // Faking VBlank, should not do this
+    // throw new Error();
+  }
   switch (opCode) {
     // Stack Push/pop
     case 0xC5: // PUSH BC
@@ -103,18 +113,6 @@ const gameloop = state => {
       }
       break;
 
-
-      // LD r8(CELA), A
-    case 0x4F: // LD C, A
-    case 0x5F: // LD E, A
-    case 0x6F: // LD L, A
-    case 0x7F: // LD A, A
-      {
-        const r8 = CELA_TABLE[opCode >> 4];
-        state.setRegister(r8, state.A);
-        addPC(1);
-      }
-      break;
 
     case 0x08: // LD (nn), SP
       {
@@ -241,6 +239,15 @@ const gameloop = state => {
       }
       break;
 
+    case 0x18: // JR r
+      {
+        const r = signed8(memory[PC + 1]);
+        log('JR r');
+        addPC(r);
+        addPC(2);
+      }
+      break;
+
     case 0x20: // JR NZ, r
     case 0xC2:
     case 0xCA:
@@ -273,14 +280,23 @@ const gameloop = state => {
       }
       break;
 
-    case 0x77: // LD (HL),A
+    case 0x77: // LD (HL), A
       {
         const HL = state.getRegister('HL');
         memory[HL] = state.A;
       }
       addPC(1);
       break;
-    case 0xAF:
+
+    case 0xEA: // LD (nn), A
+      {
+        const nn = u16(memory[PC + 2], memory[PC + 1]);
+        memory[nn] = state.getRegister('A');
+        addPC(3);
+      }
+      break;
+
+    case 0xAF: // XOR A
       xor('A', state.A);
       log('XOR A with A');
       addPC(1);
@@ -302,16 +318,110 @@ const gameloop = state => {
         setPC(RST_ADDRESS[opCode]);
       }
       break;
+      // LD r8(BDH), B
+    case 0x40: // LD B, B
+    case 0x50: // LD D, B
+    case 0x60: // LD H, B
+      {
+        const r8 = BDH_TABLE[opCode >> 4]; // check UU
+        state.setRegister(r8, state.B);
+        addPC(1);
+      }
+      break;
+      // LD r8(BDH), C
+    case 0x41: // LD B, C
+    case 0x51: // LD D, C
+    case 0x61: // LD H, C
+      {
+        const r8 = BDH_TABLE[opCode >> 4]; // check UU
+        state.setRegister(r8, state.C);
+        addPC(1);
+      }
+      break;
+      // LD r8(BDH), D
+    case 0x42: // LD B, D
+    case 0x52: // LD D, D
+    case 0x62: // LD H, D
+      {
+        const r8 = BDH_TABLE[opCode >> 4]; // check UU
+        state.setRegister(r8, state.D);
+        addPC(1);
+      }
+      break;
+      // LD r8(BDH), E
+    case 0x43: // LD B, E
+    case 0x53: // LD D, E
+    case 0x63: // LD H, E
+      {
+        const r8 = BDH_TABLE[opCode >> 4]; // check UU
+        state.setRegister(r8, state.E);
+        addPC(1);
+      }
+      break;
+      // LD r8(BDH), H
+    case 0x44: // LD B, H
+    case 0x54: // LD D, H
+    case 0x64: // LD H, H
+      {
+        const r8 = BDH_TABLE[opCode >> 4]; // check UU
+        state.setRegister(r8, state.H);
+        addPC(1);
+      }
+      break;
+      // LD r8(BDH), L
+    case 0x45: // LD B, L
+    case 0x55: // LD D, L
+    case 0x65: // LD H, L
+      {
+        const r8 = BDH_TABLE[opCode >> 4]; // check UU
+        state.setRegister(r8, state.L);
+        addPC(1);
+      }
+      break;
+      // LD r8(BDH), A
+    case 0x47: // LD B, A
+    case 0x57: // LD D, A
+    case 0x67: // LD H, A
+      {
+        const r8 = BDH_TABLE[opCode >> 4]; // check UU
+        state.setRegister(r8, state.A);
+        addPC(1);
+      }
+      break;
 
-      // LD ?, E
+      // LD r8(CELA), E
     case 0x4B: // LD C, E
-    case 0x5B:
-    case 0x6B:
-    case 0x7B:
+    case 0x5B: // LD E, E
+    case 0x6B: // LD L, E
+    case 0x7B: // LD A, E
       {
         const dstR8 = CELA_TABLE[opCode >> 4]; // check UU
         const srcR8 = 'E';
         state.setRegister(dstR8, state.getRegister(srcR8));
+        addPC(1);
+      }
+      break;
+      // LD r8(CELA), H
+    case 0x4C: // LD C, H
+    case 0x5C: // LD E, H
+    case 0x6C: // LD L, H
+    case 0x7C: // LD A, H
+      {
+        const dstR8 = CELA_TABLE[opCode >> 4]; // check UU
+        const srcR8 = 'H';
+        state.setRegister(dstR8, state.getRegister(srcR8));
+        addPC(1);
+      }
+      break;
+
+      // LD r8(CELA), A
+    case 0x4F: // LD C, A
+    case 0x5F: // LD E, A
+    case 0x6F: // LD L, A
+    case 0x7F: // LD A, A
+      {
+        const r8 = CELA_TABLE[opCode >> 4]; // check UU
+        state.setRegister(r8, state.A);
         addPC(1);
       }
       break;

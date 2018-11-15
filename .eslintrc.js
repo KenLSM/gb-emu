2dcac8f362e877bb7fd69ff5e9c3507fad5f0633
kenlsm <kenlsm@outlook.com>
2018-11-15 23:04:36 +0800

    remove drawile

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,6 @@ module.exports = {
     "no-param-reassign": 0,
     "object-curly-newline": 0,
     "implicit-arrow-linebreak": 0,
-    "no-underscore-dangle": ["error", { "allowAfterThis": true }],
+    "no-underscore-dangle": 0,
   },
 };
